1052295f9ac3a920ce399d320594e48d86413939
David Wang <dhjwang@ucdavis.edu>
2020-04-20 12:21:12 -0700

    Updates

---
+++ precache-manifest.c32df628ad3d89233435c3be844f073c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e5270ffe1e9a9974138e8ead0103ae",
+    "url": "/ACVillagers/index.html"
+  },
+  {
+    "revision": "60ae860e4e165fe9b802",
+    "url": "/ACVillagers/static/css/main.51548b3d.chunk.css"
+  },
+  {
+    "revision": "1f89321025b5e8bf5726",
+    "url": "/ACVillagers/static/js/2.87396a24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ACVillagers/static/js/2.87396a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ae860e4e165fe9b802",
+    "url": "/ACVillagers/static/js/main.0449366c.chunk.js"
+  },
+  {
+    "revision": "4a91f788de04fb453aa7",
+    "url": "/ACVillagers/static/js/runtime-main.b66ad921.js"
+  }
+]);
